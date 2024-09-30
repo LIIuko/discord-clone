c2b6dd5d98df471d9c6125fc623d6181620100fa
Konstantin <doro.kon@mail.ru>
2024-09-30 19:29:20 +0300

    Added edit server modal

--- components/modals/edit-server-modal.tsx
+++ components/modals/edit-server-modal.tsx
@@ -27,7 +27,7 @@ import { Button } from "@/components/ui/button";
 import { FileUpload } from "@/components/file-upload";
 import { useRouter } from "next/navigation";
 import { useModal } from "@/hooks/use-modal-store";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 const formSchema = z.object({
   name: z.string().min(1, {
@@ -38,11 +38,12 @@ const formSchema = z.object({
   }),
 });
 
-export const CreateServerModal = () => {
-  const { isOpen, onClose, type } = useModal();
+export const EditServerModal = () => {
+  const { isOpen, onClose, type, data } = useModal();
   const router = useRouter();
 
-  const isModalOpen = isOpen && type === "createServer";
+  const isModalOpen = isOpen && type === "editServer";
+  const { server } = data;
 
   const form = useForm({
     resolver: zodResolver(formSchema),
@@ -52,15 +53,22 @@ export const CreateServerModal = () => {
     },
   });
 
+  useEffect(() => {
+    if(server) {
+      form.setValue("name", server.name);
+      form.setValue("imageUrl", server.imageUrl);
+    }
+  }, [server, form])
+
   const isLoading = form.formState.isSubmitting;
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
     try {
-      await axios.post("/api/servers", values);
+      await axios.patch(`/api/servers/${server?.id}`, values);
 
       form.reset();
       router.refresh();
-      handleClose();
+      handleClose()
     } catch (error) {
       console.log(error);
     }
@@ -126,7 +134,7 @@ export const CreateServerModal = () => {
             </div>
             <DialogFooter className="bg-gray-100 px-6 py-4">
               <Button variant="primary" disabled={isLoading}>
-                Create
+                Save
               </Button>
             </DialogFooter>
           </form>
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -64,6 +64,7 @@ export const ServerHeader = ({
 				)}
 				{isAdmin && (
 					<DropdownMenuItem
+						onClick={() => onOpen("editServer", { server: {...server} })}
 						className="px-3 py-2 text-sm cursor-pointer"
 					>
 						Server settings
