4ad0f9f4dfa937e49d3dd1f3561e19e140bb47b9
Konstantin <doro.kon@mail.ru>
2024-10-01 13:51:12 +0300

    Added manage members

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -73,6 +73,7 @@ export const ServerHeader = ({
 				)}
 				{isAdmin && (
 					<DropdownMenuItem
+						onClick={() => onOpen("members", { server: {...server} })}
 						className="px-3 py-2 text-sm cursor-pointer"
 					>
 						Manage Members
