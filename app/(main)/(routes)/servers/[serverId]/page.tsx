aea158cfe9db0dbf1a57be76ddb2c4752ddb6751
Konstantin <doro.kon@mail.ru>
2024-10-07 10:24:51 +0300

    Added channel id page

--- app/(main)/(routes)/servers/[serverId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -1,11 +1,51 @@
-import React from 'react'
+import { auth } from '@clerk/nextjs/server';
 
-type Props = {}
+import { currentProfile } from '@/lib/current-profile'
+import { db } from '@/lib/db';
+import { redirect } from 'next/navigation';
 
-const ServerIdPage = (props: Props) => {
-  return (
-    <div>Server id page</div>
-  )
+interface ServerIdPageProps {
+  params: {
+    serverId: string,
+  }
+}
+
+const ServerIdPage = async ({
+  params
+}: ServerIdPageProps) => {
+  const profile = await currentProfile();
+
+  if(!profile){
+    return auth().redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverId,
+      members: {
+        some: {
+          profileId: profile.id,
+        }
+      }
+    },
+    include: {
+      channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        }
+      }
+    }
+  })
+
+  const initialChannel = server?.channels[0];
+
+  if(initialChannel?.name !== "general"){
+    return null;
+  }
+  return redirect(`/servers/${params.serverId}/channels/${initialChannel?.id}`)
 }
 
 export default ServerIdPage;
