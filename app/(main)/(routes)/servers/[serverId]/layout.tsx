48ff1402f90077f6b4e51a28014844bbe5a2d17a
Konstantin <doro.kon@mail.ru>
2024-10-08 18:57:39 +0300

    Socket IO setup

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -41,7 +41,7 @@ const ServerIdLayout = async ({
 			<div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0 right-0">
         <ServerMembersSidebar serverId={params.serverId} />
       </div>
-			<main className="h-full md:pl-60">
+			<main className="h-full md:px-60">
 				{children}
 			</main>
 		</div>
--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -18,7 +18,7 @@ const roleIconMap = {
 	[MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 ml-2 text-rose-500"/>
 }
 
-export const ServerMember = async ({
+export const ServerMember = ({
 	member,
 	server
 }: ServerMemberProps) => {
