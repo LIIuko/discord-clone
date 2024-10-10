24c30c53a19454a0cc09dbc1471e07a7b1578469
trader <doro.kon@mail.ru>
2024-10-10 23:02:22 +0300

    Added chat messages component

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -5,6 +5,7 @@ import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { ChatHeader } from "@/components/chat/chat-header";
 import { ChatInput } from "@/components/chat/chat-input";
+import { ChatMessages } from "@/components/chat/chat-messages";
 
 interface ChannelIdPageProps {
 	params: {
@@ -48,9 +49,20 @@ const ChannelIdPage = async ({
 				serverId={channel.serverId}
 				type="channel"
 			/>
-			<div className="flex-1">
-				Future chat
-			</div>
+			<ChatMessages
+				member={member}
+				name={channel.name}
+				chatId={channel.id}
+				type="channel"
+				apiUrl="/api/messages"
+				socketUrl="/api/socket/messages"
+				socketQuery={{
+					channelId: channel.id,
+					serverId: channel.serverId,
+				}}
+				paramKey="channelId"
+				paramValue={channel.id}
+			/>
 			<ChatInput
 				name={channel.name}
 				type="channel"
--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import { QueryProvider } from "@/components/providers/query-provide";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -32,7 +33,9 @@ export default function RootLayout({
           >
           <SocketProvider>
             <ModalProvider />
+            <QueryProvider>
             {children}
+            </QueryProvider>
           </SocketProvider>
           </ThemeProvider>
         </body>
